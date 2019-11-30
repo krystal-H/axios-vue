3a1b634e41dcdef3b86990d8b2232e21a0a8465d
krystal_H <975328916@qq.com>
2019-11-30 15:18:46 +0800

    initialize

---
+++ info.md
@@ -0,0 +1,6 @@
+#### **update-log**
+1. 通过```vue-cli3.x```创建项目
+2. 安装```axios```到项目中，```yarn add axios```,
+并添加到```package.json```依赖中
+***
+
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -8,11 +8,18 @@
 <script>
 // @ is an alias to /src
 import HelloWorld from '@/components/HelloWorld.vue'
+import axios from 'axios'
 
 export default {
   name: 'home',
   components: {
     HelloWorld
+  },
+  created() {
+    // localhost:8080访问的实际上是public下的index.html 所以接口直接/
+    axios.get('/data.json').then(res => {
+      console.log(res)
+    })
   }
 }
 </script>
